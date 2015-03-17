81dfd0cb53ffcaeaab283e7840d1dfe4081d7446
Lucas Stuyvesant <lplayer12893@aol.com>
2015-03-17 17:10:43 -0600

    Added generateCentroid.m
    first, run createMatrix on all your images, then run
    generateCentroid(matrix1, matrix2, ...)
    it accepts a variable number of arguments. It is a very general
    generation of a centroid, let me know what you think

--- createMatrix.m
+++ createMatrix.m
@@ -12,7 +12,7 @@ I = imresize(I, [768, 1024]);	% resize image to desired size
 I = rgb2gray(I);	% convert to grayscale
 BW = edge(I);	% find edges. Edges are denoted with 1, non-edges are 0
 
-X = double(BW)
+X = double(BW);
 for i = 1:size(X)	% convert to binary bipolar matrix
 	for j = 1:length(X)
 		if(X(i,j) == 0)
