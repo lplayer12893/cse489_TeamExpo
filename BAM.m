c93fd93c195226305603f553868dcd520551d2f3
Alex Mueller Poli <sarvalokakrt@gmail.com>
2015-04-01 17:47:53 -0600

    Added more documentation to BAM.m

--- BAM.m
+++ BAM.m
@@ -1,73 +1,83 @@
 function [outX, outY] = BAM(inputX, inputY)
+	% Usage: 
+	% 	inputX: the encoded image
+	% 	inputY: the encoded key value
+	% 	NOTE: Either inputX or inputY can be all zeros
+	% 
+	% 	outX: the output image
+	% 	outY: the output key value
+
 
 	% Turn the input image into a 1D vector
 	vectorDimension = size(inputX, 1) * size(inputX, 2);
 	originalDimension = size(inputX);
 	inputX = reshape(inputX, [1, vectorDimension]);
 
+	ref = generateRotationReference(384,512);
+
 	% Stored Vectors
-    bear1 = createMatrix('project_photo\bear1.jpeg');
-    bear2 = createMatrix('project_photo\bear2.jpeg');
-    bear3 = createMatrix('project_photo\bear3.jpg');
-    bear4 = createMatrix('project_photo\bear4.jpg');
-    bear5 = createMatrix('project_photo\bear5.jpg');
-    %bear6 = createMatrix('project_photo\bear6.jpg');
-    bear7 = createMatrix('project_photo\bear7.jpg');
-    bear8 = createMatrix('project_photo\bear8.jpg');
+    bear1 = createMatrix('project_photo\bear1.jpeg', ref);
+    bear2 = createMatrix('project_photo\bear2.jpeg', ref);
+    bear3 = createMatrix('project_photo\bear3.jpg', ref);
+    bear4 = createMatrix('project_photo\bear4.jpg', ref);
+    bear5 = createMatrix('project_photo\bear5.jpg', ref);
+    %bear6 = createMatrix('project_photo\bear6.jpg', ref);
+    bear7 = createMatrix('project_photo\bear7.jpg', ref);
+    bear8 = createMatrix('project_photo\bear8.jpg', ref);
 
-    elephant1 = createMatrix('project_photo\elephant1.jpg');
-    elephant2 = createMatrix('project_photo\elephant2.jpg');
-    elephant3 = createMatrix('project_photo\elephant3.jpg');
-    elephant4 = createMatrix('project_photo\elephant4.jpg');
-    elephant5 = createMatrix('project_photo\elephant5.jpg');
-    elephant6 = createMatrix('project_photo\elephant6.jpg');
-    elephant7 = createMatrix('project_photo\elephant7.jpg');
-    elephant8 = createMatrix('project_photo\elephant8.jpg');
+    elephant1 = createMatrix('project_photo\elephant1.jpg', ref);
+    elephant2 = createMatrix('project_photo\elephant2.jpg', ref);
+    elephant3 = createMatrix('project_photo\elephant3.jpg', ref);
+    elephant4 = createMatrix('project_photo\elephant4.jpg', ref);
+    elephant5 = createMatrix('project_photo\elephant5.jpg', ref);
+    elephant6 = createMatrix('project_photo\elephant6.jpg', ref);
+    elephant7 = createMatrix('project_photo\elephant7.jpg', ref);
+    elephant8 = createMatrix('project_photo\elephant8.jpg', ref);
 
-    fighter1 = createMatrix('project_photo\fighter1.jpg');
-    fighter2 = createMatrix('project_photo\fighter2.jpg');
-    fighter3 = createMatrix('project_photo\fighter3.jpg');
-    fighter4 = createMatrix('project_photo\fighter4.jpg');
-    fighter5 = createMatrix('project_photo\fighter5.jpg');
-    fighter6 = createMatrix('project_photo\fighter6.jpg');
-    fighter7 = createMatrix('project_photo\fighter7.jpg');
-    fighter8 = createMatrix('project_photo\fighter8.jpg');
+    fighter1 = createMatrix('project_photo\fighter1.jpg', ref);
+    fighter2 = createMatrix('project_photo\fighter2.jpg', ref);
+    fighter3 = createMatrix('project_photo\fighter3.jpg', ref);
+    fighter4 = createMatrix('project_photo\fighter4.jpg', ref);
+    fighter5 = createMatrix('project_photo\fighter5.jpg', ref);
+    fighter6 = createMatrix('project_photo\fighter6.jpg', ref);
+    fighter7 = createMatrix('project_photo\fighter7.jpg', ref);
+    fighter8 = createMatrix('project_photo\fighter8.jpg', ref);
 
-    passenger1 = createMatrix('project_photo\passenger1.jpg');
-    passenger2 = createMatrix('project_photo\passenger2.jpg');
-    passenger3 = createMatrix('project_photo\passenger3.jpg');
-    passenger4 = createMatrix('project_photo\passenger4.jpg');
-    passenger5 = createMatrix('project_photo\passenger5.jpg');
-    passenger6 = createMatrix('project_photo\passenger6.jpg');
-    passenger7 = createMatrix('project_photo\passenger7.jpg');
-    passenger8 = createMatrix('project_photo\passenger8.jpg');
+    passenger1 = createMatrix('project_photo\passenger1.jpg', ref);
+    passenger2 = createMatrix('project_photo\passenger2.jpg', ref);
+    passenger3 = createMatrix('project_photo\passenger3.jpg', ref);
+    passenger4 = createMatrix('project_photo\passenger4.jpg', ref);
+    passenger5 = createMatrix('project_photo\passenger5.jpg', ref);
+    passenger6 = createMatrix('project_photo\passenger6.jpg', ref);
+    passenger7 = createMatrix('project_photo\passenger7.jpg', ref);
+    passenger8 = createMatrix('project_photo\passenger8.jpg', ref);
 
-    ship1 = createMatrix('project_photo\ship1.jpg');
-    ship2 = createMatrix('project_photo\ship2.jpg');
-    ship3 = createMatrix('project_photo\ship3.jpg');
-    ship4 = createMatrix('project_photo\ship4.jpg');
-    ship5 = createMatrix('project_photo\ship5.jpg');
-    ship6 = createMatrix('project_photo\ship6.jpg');
-    ship7 = createMatrix('project_photo\ship7.jpg');
-    ship8 = createMatrix('project_photo\ship8.jpg');
+    ship1 = createMatrix('project_photo\ship1.jpg', ref);
+    ship2 = createMatrix('project_photo\ship2.jpg', ref);
+    ship3 = createMatrix('project_photo\ship3.jpg', ref);
+    ship4 = createMatrix('project_photo\ship4.jpg', ref);
+    ship5 = createMatrix('project_photo\ship5.jpg', ref);
+    ship6 = createMatrix('project_photo\ship6.jpg', ref);
+    ship7 = createMatrix('project_photo\ship7.jpg', ref);
+    ship8 = createMatrix('project_photo\ship8.jpg', ref);
 
-    sub1 = createMatrix('project_photo\sub1.jpg');
-    sub2 = createMatrix('project_photo\sub2.jpg');
-    sub3 = createMatrix('project_photo\sub3.jpg');
-    sub4 = createMatrix('project_photo\sub4.jpg');
-    sub5 = createMatrix('project_photo\sub5.jpg');
-    sub6 = createMatrix('project_photo\sub6.jpg');
-    sub7 = createMatrix('project_photo\sub7.jpg');
-    % sub8 = createMatrix('project_photo\sub8.jpg');
+    sub1 = createMatrix('project_photo\sub1.jpg', ref);
+    sub2 = createMatrix('project_photo\sub2.jpg', ref);
+    sub3 = createMatrix('project_photo\sub3.jpg', ref);
+    sub4 = createMatrix('project_photo\sub4.jpg', ref);
+    sub5 = createMatrix('project_photo\sub5.jpg', ref);
+    sub6 = createMatrix('project_photo\sub6.jpg', ref);
+    sub7 = createMatrix('project_photo\sub7.jpg', ref);
+    % sub8 = createMatrix('project_photo\sub8.jpg', ref);
 
-    whale1 = createMatrix('project_photo\whale1.jpg');
-    whale2 = createMatrix('project_photo\whale2.jpg');
-    whale3 = createMatrix('project_photo\whale3.jpg');
-    whale4 = createMatrix('project_photo\whale4.jpg');
-    whale5 = createMatrix('project_photo\whale5.jpg');
-    whale6 = createMatrix('project_photo\whale6.jpg');
-    whale7 = createMatrix('project_photo\whale7.jpg');
-    whale8 = createMatrix('project_photo\whale8.jpg');
+    whale1 = createMatrix('project_photo\whale1.jpg', ref);
+    whale2 = createMatrix('project_photo\whale2.jpg', ref);
+    whale3 = createMatrix('project_photo\whale3.jpg', ref);
+    whale4 = createMatrix('project_photo\whale4.jpg', ref);
+    whale5 = createMatrix('project_photo\whale5.jpg', ref);
+    whale6 = createMatrix('project_photo\whale6.jpg', ref);
+    whale7 = createMatrix('project_photo\whale7.jpg', ref);
+    whale8 = createMatrix('project_photo\whale8.jpg', ref);
 
     % Xbear = reshape(generateCentroid(bear1, bear2, bear3, bear4, bear5, bear7, bear8), [1, vectorDimension]);
     % Xelephant = reshape(generateCentroid(elephant1, elephant2, elephant3, elephant4, elephant5, elephant6, elephant7), [1, vectorDimension]);
@@ -93,14 +103,12 @@ function [outX, outY] = BAM(inputX, inputY)
 	Ysub = 			[-1 -1 -1 -1 -1 1 -1];
 	Ywhale = 		[-1 -1 -1 -1 -1 -1 1];
 
-	% Xstored = [Xbear; Xelephant; Xfighter; Xpassenger; Xship; Xsub; Xwhale];
-	% Ystored = [Ybear; Yelephant; Yfighter; Ypassenger; Yship; Ysub; Ywhale];
-
-	Xstored = [Xbear; Xelephant];
-	Ystored = [Ybear; Yelephant];
+	Xstored = [Xbear; Xelephant; Xfighter; Xpassenger; Xship; Xsub; Xwhale];
+	Ystored = [Ybear; Yelephant; Yfighter; Ypassenger; Yship; Ysub; Ywhale];
 
 	% Weight Matrix Calculation
 	W = CalcWeight(Xstored, Ystored);
+	size(W)
 
 	% Apply activation function until convergence
 	testX = inputX;
@@ -117,6 +125,7 @@ function [outX, outY] = BAM(inputX, inputY)
 		testX = actX;
 		actX = Activation(actY*W', actX);
 
+		% Check for convergence
 		if isequal(testX, actX) && isequal(testY, actY)
 			break;
 		end
@@ -125,8 +134,6 @@ function [outX, outY] = BAM(inputX, inputY)
 	% Convert the 1D vector baack into the matrix representing the image
 	outX = reshape(actX, originalDimension);
 	outY = actY;
-
-	% imshow(outX);
 
 end
 
@@ -149,7 +156,8 @@ function out = CalcWeight(x, y)
 	
 	output = zeros(size(x,2), size(y,2));
 
-	% NOTE: we are assuming the number of rows in x is equal to the number of rows in y
+	% NOTE: we are assuming the number of rows in x is equal to the number of rows in y.
+	% This should always be true when the network is being used as intended.
 	for i = 1:size(x,1)
 		output = output + x(i,:).' * y(i,:);
 	end
