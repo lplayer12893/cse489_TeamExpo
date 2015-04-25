1427cccaedd46fc9bb18ac5c1804f8e193596c12
bbhakta <bhavik1292@gmail.com>
2015-04-25 16:29:23 -0600

    Create readInputs.m

---
+++ final_project/readInputs.m
@@ -0,0 +1,17 @@
+function [M, A] = readInputs(file)
+
+usage = {'USAGE: readInputs(file)';'* file is the path to the csv file'};
+if(exist(file) == 0)
+disp(usage);
+return
+end
+disp('Creating Matrix');
+
+%reads the input file and it separtes the data and classification
+read = csvread(file, 1, 0);
+y = read(:,1:14);
+z= read(:,18:size(read,2));
+M = [y z];
+A= read(:,15:17);
+
+
