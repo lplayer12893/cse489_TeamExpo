4137b1ce14cc3db679a6afa175ff4836e0ef57ef
Alex Mueller Poli <sarvalokakrt@gmail.com>
2015-05-04 16:51:54 -0600

    Fixed another bug with reading encoded data

--- final_project/readEncodedData.m
+++ final_project/readEncodedData.m
@@ -9,5 +9,5 @@ disp('Creating Matrix');
 
 %reads the input file and it separates the data and classification
 read = csvread(file, 1, 1);
-M = read(:,1:510);
-A = read(:,511:513);
+M = read(:,1:509);
+A = read(:,510:512);
