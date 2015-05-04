28d6251ff4499df77c98c2e6d9a7fc5920e91c88
Lucas Stuyvesant <lplayer12893@aol.com>
2015-05-04 17:03:32 -0600

    only 513 columns

--- final_project/writeEncodedData.m
+++ final_project/writeEncodedData.m
@@ -6,20 +6,29 @@ suffix = '_EncodedData.csv';
 cprefixes = {'Buy','Wait','Sell'};
 csuffix = '_Centroid';
 
-[M,A,conf] = getEncodedData(10);
+%[M,A,conf] = getEncodedData(10);
 
-s = size(M);
+%s = size(M);
 
-for i = 1:s(2)
-    writeOutputs(M{i},A{i},[prefixes{i} suffix]);
+%for i = 1:s(2)
+%    writeOutputs(M{i},A{i},[prefixes{i} suffix]);
+%end
+
+% generalize conf for centroids
+conf = loadCorrelations(10);
+
+M = cell(size(prefixes));
+A = cell(size(prefixes));
+
+for k = 1:size(prefixes)
+    [m,a] = readEncodedData([prefixes{k} suffix]);
+    M{k} = m;
+    A{k} = a;
 end
 
-c = generateCentroid(M,A,conf);
-
-s2 = size(c)
-
-for j = 1:s2(2)
-    csvwrite([cprefixes{j} csuffix], c(i,:));
+for j = 1:size(cprefixes)
+    c = generateCentroid(M,A,conf);
+    csvwrite([cprefixes{j} csuffix], c(j,:));
 end
 
 A = 0;
